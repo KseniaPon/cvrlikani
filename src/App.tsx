aec4e29a6e2b3a2997c7058b17c95e900cf879be
Ksenia Ponomareva <ponomarevaks@gmail.com>
2024-06-13 14:56:55 +0200

    feat: TextFiled form sends data to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
+import './App.css';
+import { TextField, MessageData } from './components/TextField';
 
+// const texts = [
+//     {
+//         name: 'Lenka Nová',
+//         text: 'Ahoj! Jsem Lenka!',
+//     },
+// ];
 function App() {
-
-  return (
-    <>
-      <h1>New branch test</h1>
-    </>
-  )
+    const handleSubmitForm = (twitter: MessageData) => {
+        console.log(twitter);
+    };
+    return (
+        <>
+            <h1>Cvrlikání</h1>
+            <TextField onSubmit={handleSubmitForm} />
+        </>
+    );
 }
 
-export default App
+export default App;
